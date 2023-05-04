fbf44a01d8eecba71473379715ccbcc66ba27f5c
Kyle Ross <107285758+Kyle-Ross@users.noreply.github.com>
2023-05-04 19:34:55 +1000

    Initial commit

---
+++ Main.py
@@ -0,0 +1,181 @@
+import pandas as pd
+import datetime
+import json
+import os
+
+# Target folder path
+census_folder_path = r"\2021_GCP_all_for_AUS_short-header"
+
+# Config file location
+config_file = r"config.csv"
+
+# Function to pretty print dictionaries
+def print_dict(target_dict):
+    # Deals with dataframes in the dict
+    def if_df_in_dict(obj):
+        if isinstance(obj, pd.DataFrame):
+            return "DataFrame Object"
+        raise TypeError(f"{type(obj)} not supported")
+
+    print(json.dumps(target_dict, default=if_df_in_dict, indent=4))
+
+
+# Function to get dictionaries containing information on each file in the census folder
+def census_file_info(folder_path):
+    # Get all file paths in the target folder
+    file_paths = []
+    for root, directories, files in os.walk(folder_path):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            # Split the filename into its name and type components
+            name, file_type = os.path.splitext(filename)
+            # Split the name by '_'
+            name_parts = name.split('_')
+            # Split the path into its components
+            path_parts = os.path.split(file_path)
+            # Create a dictionary with the path components as key-value pairs
+            file_dict = {
+                'filename': name,
+                'nameparts':
+                    {
+                        "census_desc": name_parts[0],
+                        "file_code": name_parts[1],
+                        "country": name_parts[2],
+                        "geo_type": name_parts[3]
+                    },
+                'filetype': file_type,
+                'directory': path_parts[0],
+                'full_path': file_path
+            }
+            # Add the dictionary to the list
+            file_paths.append(file_dict)
+
+    # Returning the result
+    return file_paths
+
+
+# Function to gather, filter & join specified census files
+def accumulate_census(target_folder_path,  # Where the census folder is
+                      config_path,  # Where the config folder is saved
+                      geo_type,  # What spatial aggregation sub-folder to target
+                      output_folder='',  # Set the location of the output folder, will be the script location by default
+                      col_desc='short',  # Can be 'short' or 'long'
+                      col_affix='prefix'):  # Affix a 'prefix', 'suffix' or 'none' of the csv's file code to each col
+    # Set the output folder to be a sub-folder of the script folder if unchanged
+    if output_folder == '':
+        output_folder = os.path.dirname(os.path.abspath(__file__))
+
+    # Getting the list of dictionaries with file info
+    file_dicts = census_file_info(target_folder_path)
+
+    # Getting config file
+    config_data = pd.read_csv(config_path)
+
+    # List of unique data file codes in the config
+    distinct_file_codes = config_data['DATA_FILE_CODE'].unique().tolist()
+
+    # Reducing the list to only the target csvs based on the config file
+    targeted_file_paths = []
+
+    for file_info_dict in file_dicts:
+        file_info_dict['nameparts']['file_code']
+        if file_info_dict['nameparts']['geo_type'] == geo_type and \
+                file_info_dict['nameparts']['file_code'] in distinct_file_codes and \
+                file_info_dict['filetype'] == ".csv":
+            targeted_file_paths.append(file_info_dict)
+
+    # Looping through the dictionaries, reading and filtering the resulting dataframes
+    for file_dict in targeted_file_paths:
+        # Getting the file path
+        file_path = file_dict['full_path']
+
+        # Opening the csv as a df
+        unfiltered_df = pd.read_csv(file_path)
+
+        # Saving the unfiltered df to the dict
+        file_dict["unfiltered_df"] = unfiltered_df
+
+        # Creating a list of column names to keep from the config file, where index 0 is the old name and 1 is the new
+        col_list_list = config_data[config_data['DATA_FILE_CODE'] == file_dict['nameparts']['file_code']] \
+            [['FIELD_SHORT', 'FIELD_LONG']].drop_duplicates().values.tolist()
+
+        # Looping through col list and putting it in a dictionary
+        col_dict = {}
+
+        for cols_list in col_list_list:
+            # Getting col names
+            old_col_name = cols_list[0]
+
+            # Setting the replacement column name conditionally depending on arguments
+            if col_desc == 'short':
+                new_col_name = old_col_name
+            elif col_desc == 'long':
+                new_col_name = cols_list[1]
+            else:
+                print("col_desc must be either 'short or 'long' - incorrect value entered. Reverting to short.")
+                new_col_name = old_col_name
+
+            # Adding a prefix or suffix depending on arguments
+            if col_affix == 'prefix':
+                new_col_name = file_dict['nameparts']['file_code'] + "_" + new_col_name
+            elif col_affix == 'suffix':
+                new_col_name = new_col_name + "_" + file_dict['nameparts']['file_code']
+            elif col_affix == 'none':
+                # Leave var unchanged
+                new_col_name = new_col_name
+            else:
+                print("col_desc must be 'prefix', 'suffix' or 'none' - incorrect value entered. Reverting to none.")
+                # Do nothing
+
+            # Adding the old and new key combination to the dictionary
+            col_dict[old_col_name] = new_col_name
+
+        # Getting a list with just the old col name
+        old_col_list = [x[0] for x in col_list_list]
+
+        # Appending the target columns to the dictionary
+        file_dict["target_columns"] = col_dict
+
+        # Establishing the name of the primary key column
+        primary_key_col = str(geo_type) + "_CODE_2021"
+
+        # Adding that to the list of old columns which is used to filter below
+        old_col_list.insert(0, primary_key_col)
+
+        # Renaming and filtering columns using the config data
+        filtered_df = unfiltered_df.loc[:, old_col_list].rename(columns=col_dict)
+
+        # Saving the filtered df to the dict
+        file_dict["filtered_df"] = filtered_df
+
+    # Adding the filtered dataframes to a list
+    filtered_df_list = []
+
+    for a_dict in targeted_file_paths:
+        filtered_df_list.append(a_dict["filtered_df"])
+
+    # Merging the dataframes together
+    # Create an empty dataframe to store the merged data
+    merged_df = pd.DataFrame()
+
+    # Loop through each dataframe in the list and merge with the 'merged_df'
+    for df in filtered_df_list:
+        if merged_df.empty:
+            merged_df = df
+        else:
+            merged_df = pd.merge(merged_df, df, on=primary_key_col, validate='one_to_one')
+
+    # Create file name
+    current_dt = datetime.datetime.now().strftime("%Y-%m-%d %H-%M")
+    output_file_name = "Census Data" + "-" + geo_type + "_" + col_desc + "_" + col_affix + "-" + current_dt + ".csv"
+
+    # Output the csv
+    merged_df.to_csv(os.path.join(output_folder, output_file_name))
+
+
+# Calling the function
+accumulate_census(census_folder_path,
+                  config_file,
+                  'LGA',
+                  col_desc='long',
+                  col_affix='prefix')
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # abs-census-merger
 A Python script which takes the census data pack from the ABS and instantly merges data across the many separate files. Allows you to specify files, fields and spatial aggregation against customizable settings in a config csv file.
+
+## How to use
+
+1. Download your desired [datapack](https://www.abs.gov.au/census/find-census-data/datapacks) on the ABS site
+2. Clone this repo
+3. Adjust desired files and fields in the config file
+4. Open Main.py in your desired IDE
+5. Set the path of the downloaded and extracted census folder at the top of the script with the `census_folder_path` variable
+6. Adjust the function parameters as needed at the bottom of the script with the pre-written function call
+7. Run the script
+
+```Python
+def accumulate_census(target_folder_path,  # Where the census folder is
+                      config_path,  # Where the config folder is saved
+                      geo_type,  # What spatial aggregation sub-folder to target
+                      output_folder='',  # Set the location of the output folder, will be the script location by default
+                      col_desc='short',  # Can be 'short' or 'long'
+                      col_affix='prefix'):  # Affix a 'prefix', 'suffix' or 'none' of the csv's file code to each col
+```
+
+## Notes
+
+- Example output is included
+- Geo types include LGA, POA, SA1, SA2 etc
+- col_desc will determine whether column names are change via the config file
+- Use the metadata .xlsx file included in the datapack download to set up the config file with the field you want
+- Downloading the databack to the script folder will mean you don't need to update the path references
